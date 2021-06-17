424f57fa43d19100d8d750e2c0cdb42b31b201b6
Michael Chow <mc_al_github@fastmail.com>
2021-06-17 19:19:20 -0400

    fix: expose tests module with find_namespace_packages

--- setup.py
+++ setup.py
@@ -2,28 +2,28 @@
 
 import re
 import ast
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 
-_version_re = re.compile(r'__version__\s+=\s+(.*)')
+_version_re = re.compile(r"__version__\s+=\s+(.*)")
 
-with open('calitp/__init__.py', 'rb') as f:
-    version = str(ast.literal_eval(_version_re.search(
-        f.read().decode('utf-8')).group(1)))
+with open("calitp/__init__.py", "rb") as f:
+    version = str(
+        ast.literal_eval(_version_re.search(f.read().decode("utf-8")).group(1))
+    )
 
 setup(
-    name='calitp',
+    name="calitp",
     version=version,
-    packages=find_packages(),
+    packages=find_namespace_packages(),
     install_requires=[
         "gcsfs",
         "pandas",
         "pandas-gbq",
         "pybigquery",
-        "google-cloud-bigquery"
-        ],
-    description='',
-    author='',
-    author_email='',
-    url='https://github.com/cal-itp/calitp-py'
-    )
-
+        "google-cloud-bigquery",
+    ],
+    description="",
+    author="",
+    author_email="",
+    url="https://github.com/cal-itp/calitp-py",
+)
