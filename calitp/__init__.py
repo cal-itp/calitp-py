0e73b557cd06ef9157f1bfff891b1da5fbcfda6b
Michael Chow <mc_al_github@fastmail.com>
2021-10-04 09:28:49 -0400

    release: v0.0.5

--- calitp/__init__.py
+++ calitp/__init__.py
@@ -1,6 +1,6 @@
 # flake8: noqa
 
-__version__ = "0.0.4"
+__version__ = "0.0.5"
 
 from .sql import get_table, write_table, query_sql, to_snakecase, get_engine
 from .storage import save_to_gcfs, read_gcfs
