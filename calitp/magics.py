8e9a40771c9c0310b6345c3bdc7cb502dff7141c
Natalya Diaz <natalyadiaz@Natalyas-MacBook-Pro-2.local>
2021-10-22 10:59:00 -0400

    feat:calitp-magics: added calitp magics for sql queries in python notebook

--- calitp/magics.py
+++ calitp/magics.py
@@ -1,3 +1,6 @@
-from IPython.core.magic import (register_line_magic, register_cell_magic,
-                                register_line_cell_magic)
+from IPython.core.magic import (register_cell_magic)
+from calitp.sql import query_sql
 
+@register_cell_magic
+def sql(line, cell):
+    return query_sql(cell)
