7352e0be438c23e4f7a144f73c545455e873525a
Charles Costanzo <cvc5185@gmail.com>
2022-04-06 11:40:54 -0400

    calitp-py(version): bumped up version number to attempt resolution of commit association

--- calitp/__init__.py
+++ calitp/__init__.py
@@ -1,6 +1,6 @@
 # flake8: noqa
 
-__version__ = "0.0.13"
+__version__ = "0.0.15"
 
 from .sql import get_table, write_table, query_sql, to_snakecase, get_engine
 from .storage import save_to_gcfs, read_gcfs
