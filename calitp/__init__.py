ee8ded89d596e9cf178af104923ace6a3300424f
Andrew Vaccaro <atvaccaro@gmail.com>
2022-04-14 14:21:18 -0400

    bump version

--- calitp/__init__.py
+++ calitp/__init__.py
@@ -1,6 +1,6 @@
 # flake8: noqa
 
-__version__ = "0.0.15"
+__version__ = "0.0.16"
 
 from .sql import get_table, write_table, query_sql, to_snakecase, get_engine
 from .storage import save_to_gcfs, read_gcfs
