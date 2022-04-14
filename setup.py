1a5980610fb32e38e951520fdcb336b946a776d3
Andrew Vaccaro <atvaccaro@gmail.com>
2022-04-14 14:27:16 -0400

    fmt

--- setup.py
+++ setup.py
@@ -7,9 +7,7 @@ from setuptools import setup, find_namespace_packages
 _version_re = re.compile(r"__version__\s+=\s+(.*)")
 
 with open("calitp/__init__.py", "rb") as f:
-    version = str(
-        ast.literal_eval(_version_re.search(f.read().decode("utf-8")).group(1))
-    )
+    version = str(ast.literal_eval(_version_re.search(f.read().decode("utf-8")).group(1)))
 
 setup(
     name="calitp",
