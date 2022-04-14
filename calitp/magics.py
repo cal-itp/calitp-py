83d55fadd0ea1a59dd0a6e373fbddae87bea767b
Andrew Vaccaro <atvaccaro@gmail.com>
2022-04-14 12:19:32 -0400

    probably very hacky way to add papermill parameters at runtime

--- calitp/magics.py
+++ calitp/magics.py
@@ -1,3 +1,4 @@
+import json
 from IPython.core.magic import register_cell_magic
 from IPython.core.magic_arguments import argument, magic_arguments, parse_argstring
 from IPython.display import Markdown, display
@@ -35,3 +36,16 @@ def sql(line, cell):
 
     if not args.quiet:
         return res
+
+
+@register_cell_magic
+def capture_parameters(line, cell):
+    shell = get_ipython()
+    shell.run_cell(cell, silent=True)
+    # We assume the last line is a tuple
+    tup = [s.strip() for s in cell.strip().split("\n")[-1].split(",")]
+
+    print(json.dumps({
+        identifier: shell.user_ns[identifier]
+        for identifier in tup if identifier
+    }))
