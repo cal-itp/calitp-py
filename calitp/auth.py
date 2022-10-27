46bb340948e87f9949095a9ff85ec2648576f71d
Andrew Vaccaro <atvaccaro@gmail.com>
2022-10-27 12:42:35 -0400

    actually load secrets by label

--- calitp/auth.py
+++ calitp/auth.py
@@ -1,5 +1,5 @@
 import os
-from typing import Mapping, Sequence
+from typing import Mapping
 
 import google_crc32c
 from google.cloud import secretmanager
@@ -8,26 +8,33 @@ AUTH_KEYS_ENV_VAR = "CALITP_AUTH_KEYS"
 DEFAULT_AUTH_KEYS = tuple(os.environ[AUTH_KEYS_ENV_VAR].split(",")) if AUTH_KEYS_ENV_VAR in os.environ else tuple()
 
 
-def get_secrets(
-    keys: Sequence[str] = DEFAULT_AUTH_KEYS,
-    secret_client=secretmanager.SecretManagerServiceClient(),
+def get_secrets_with_label(
+    label: str,
+    client=secretmanager.SecretManagerServiceClient(),
 ) -> Mapping[str, str]:
-    secrets = {}
+    project = "projects/cal-itp-data-infra"
 
-    for key in keys:
-        name = f"projects/cal-itp-data-infra/secrets/{key}/versions/latest"
-        response = secret_client.access_secret_version(request={"name": name})
+    filter_request = {
+        "parent": project,
+        "filter": f"labels.{label}:*",
+    }
+
+    secret_values = {}
+
+    for secret in client.list_secrets(request=filter_request):
+        version = f"{secret.name}/versions/latest"
+        response = client.access_secret_version(request={"name": version})
 
         crc32c = google_crc32c.Checksum()
         crc32c.update(response.payload.data)
         if response.payload.data_crc32c != int(crc32c.hexdigest(), 16):
-            raise ValueError(f"Data corruption detected for secret {name}.")
+            raise ValueError(f"Data corruption detected for secret {version}.")
 
-        secrets[key] = response.payload.data.decode("UTF-8").strip()
+        secret_values[secret.name.split("/")[-1]] = response.payload.data.decode("UTF-8").strip()
 
-    return secrets
+    return secret_values
 
 
 if __name__ == "__main__":
     print("loading secrets...")
-    print(get_secrets().keys())
+    print(get_secrets_with_label("gtfs_rt").keys())
