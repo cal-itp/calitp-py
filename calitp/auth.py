fcecc8e232530da1b8e3a6a001969f3c41a171eb
Andrew Vaccaro <atvaccaro@gmail.com>
2023-02-02 11:42:47 -0500

    make pre-commit pass

--- calitp/auth.py
+++ calitp/auth.py
@@ -9,7 +9,6 @@ def get_secret_by_name(
     name: str,
     client=secretmanager.SecretManagerServiceClient(),
 ) -> str:
-
     version = f"{project}/secrets/{name}/versions/latest"
     response = client.access_secret_version(version)
     return response.payload.data.decode("UTF-8").strip()
@@ -19,7 +18,6 @@ def get_secrets_by_label(
     label: str,
     client=secretmanager.SecretManagerServiceClient(),
 ) -> Mapping[str, str]:
-
     secret_values = {}
 
     # once we get on at least 2.0.0 of secret manager, we can filter server-side
