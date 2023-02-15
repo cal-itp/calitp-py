ccd01cb6df6a373e29717ed84c609296551e8658
Andrew Vaccaro <atvaccaro@gmail.com>
2023-02-15 10:25:22 -0800

    address PR comments

--- calitp-data-analysis/tests/helpers.py
+++ calitp-data-analysis/tests/helpers.py
@@ -5,6 +5,11 @@ CI_SCHEMA_NAME = "calitp_py"
 
 @contextmanager
 def as_calitp_user(user):
+    """
+    Sets the CALITP_USER env var to a given user string
+    temporarily for tests.
+    :param user:
+    """
     import os
 
     prev_user = os.environ.get("CALITP_USER")
