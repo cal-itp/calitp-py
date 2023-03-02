517b73d5241201812e7210b741859418e5084360
Andrew Vaccaro <atvaccaro@gmail.com>
2023-03-02 14:37:49 -0500

    mvp saving dataframes in partitioned manner

--- calitp-data-analysis/calitp_data_analysis/storage.py
+++ calitp-data-analysis/calitp_data_analysis/storage.py
@@ -1,21 +1,49 @@
 import os
 from enum import Enum
 from functools import partial
-from typing import List
+from typing import Any, Dict, List, Union
 
-import pandas as pd
 import geopandas as gpd
+import pandas as pd
+import pendulum
 from pandas import util
 
 # gs://calitp-publish-data-analysis
 CALITP_BUCKET__PUBLISH_DATA_ANALYSIS = os.getenv("CALITP_BUCKET__PUBLISH_DATA_ANALYSIS")
 
-class FileType(Enum):
-    csv = 'csv'
-    jsonl = 'jsonl'
-    geojson = 'geojson'
 
-def save_partitioned_dataframe(df: Union[pd.DataFrame, gpd.DataFrame], table: str, partition_columns: List[str], filetype='csv', bucket: str = None, **write_kwargs) -> List[str]
+class FileType(Enum):
+    csv = "csv"
+    jsonl = "jsonl"
+    geojson = "geojson"
+
+
+# copied from calitp-data-infra
+PARTITION_SERIALIZERS = {
+    str: str,
+    int: str,
+    pendulum.Date: lambda d: d.to_date_string(),
+    pendulum.DateTime: lambda dt: dt.to_iso8601_string(),
+}
+
+
+def convert_pandas_type(item: Any) -> Union[str, int, pendulum.Date, pendulum.DateTime]:
+    if isinstance(item, (str, int)):
+        return item
+    if isinstance(item, pd.Timestamp):
+        return pendulum.instance(item)
+    raise RuntimeError(f"invalid type {type(item)} for serialization")
+
+
+def save_partitioned_dataframe(
+    df: Union[pd.DataFrame, gpd.GeoDataFrame],
+    table: str,
+    partition_columns: List[str],
+    filetype="csv",
+    bucket: str = None,
+    filename: str = "data",
+    **write_kwargs,
+) -> Dict[str, str]:
     if not bucket:
         bucket = CALITP_BUCKET__PUBLISH_DATA_ANALYSIS
 
@@ -24,23 +52,42 @@ def save_partitioned_dataframe(df: Union[pd.DataFrame, gpd.DataFrame], table: st
 
     if not bucket.startswith("gs://"):
         bucket = f"gs://{bucket}"
-    df.to_file(geojsonl_fpath, driver="GeoJSONSeq")
+
+    paths = {}
     grouped = df.groupby(partition_columns)
 
     for name, group in grouped:
-        if filetype == 'csv':
+        if filetype == "csv":
             write_func = partial(group.to_csv, **write_kwargs)
-            extension = '.csv'
-        elif filetype == 'json':
-            write_func = partial(group.to_json, orient='records', lines=True, **write_kwargs)
-            extension = '.jsonl'
+            extension = ".csv"
+        elif filetype == "json":
+            write_func = partial(group.to_json, orient="records", lines=True, **write_kwargs)
+            extension = ".jsonl"
         else:
             raise ValueError(f"unknown filetype {filetype}")
         # I'm manually implementing this here but we probably should be handling this via PartitionedGCSArtifact
-        write_func(path=f"{CALITP_BUCKET__PUBLISH_DATA_ANALYSIS}/{table}/{extension}")
+        if len(partition_columns) == 1:
+            partition_path = f"{partition_columns[0]}={PARTITION_SERIALIZERS[type(name)](name)}"
+        else:
+            partition_path = "/".join(
+                f"{key}={PARTITION_SERIALIZERS[type(value)](value)}"
+                for key, value in zip(partition_columns, [convert_pandas_type(elem) for elem in name])
+            )
+        path = f"{bucket.rstrip('/')}/{table}/{partition_path}/{filename}{extension}"
+        print(f"Saving {name} to {path}")
+        write_func(path_or_buf=path)
+        paths[str(name)] = path
+    return paths
 
-if __name__ == '__main__':
-    save_partitioned_dataframe(df=util.testing.makeMixedDataFrame(),
-                               table="mixed_data_frame",
 
-                               )
+if __name__ == "__main__":
+    save_partitioned_dataframe(
+        df=util.testing.makeMixedDataFrame(),
+        table="mixed_data_frame",
+        partition_columns=["C"],
+    )
+    save_partitioned_dataframe(
+        df=util.testing.makeMixedDataFrame(),
+        table="mixed_data_frame_multiple",
+        partition_columns=["C", "D"],
+    )
